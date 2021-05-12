d224d937b725ae43128d279d690a93e6b59e8a6f
DanaLooschelders <dana.looschelders@uni-muenster.de>
2021-05-12 22:28:14 +0200

    test of uploading rdata

--- setupGithub.r
+++ setupGithub.r
@@ -1,2 +1,6 @@
 library(usethis)
 use_git_config(user.name="NAME", user.mail="EMAIL")
+
+#oder in der Shell
+#C:\Users\Dana>git config --global user.email "EMAIL"
+#C:\Users\Dana>git config --global user.name "NAME"
