03e4d11707850a5f3d9c5f69a17d958c1c32e363
DanaLooschelders <dana.looschelders@uni-muenster.de>
2021-06-13 16:55:57 +0200

    modify crop

--- calculate_BT_from_Landsat.R
+++ calculate_BT_from_Landsat.R
@@ -6,6 +6,8 @@ library(sp)
 library(rgdal)
 library(gdalUtils)
 setwd("/Users/amelie/Desktop/LOEK/MSc/M8/Projekt/Sciebo")
+setwd("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung")
+
 LS10 <- raster("Daten_roh/Landsat/LC08_L1TP_197024_20200724_20200911_02_T1/LC08_L1TP_197024_20200724_20200911_02_T1_B10.TIF")
 LS11 <- raster("Daten_roh/Landsat/LC08_L1TP_197024_20200724_20200911_02_T1/LC08_L1TP_197024_20200724_20200911_02_T1_B11.TIF")
 
@@ -19,11 +21,12 @@ crs(LS10)
 
 gadm_sf <- st_transform(gadm_sf, crs = st_crs(LS10))
 
-LS10_crop <- crop(gadm_sf,LS10)
+LS10_crop <- crop(y=gadm_sf,x=LS10)
+
 e <- extent(395103.5,415705.1,5744177,5768658)
 LS10_crop <- crop(LS10,e)
 extent(gadm_sf)
 extent(LS10)
-mapview(LS10)
+mapview(LS10_crop)
 
 BT(Landsat_10 = LS10 , Landsat_11 = LS11)
