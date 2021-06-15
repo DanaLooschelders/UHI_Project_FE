23bc6ee199e4eafa88437c5d88ec0f3dbe9835cc
DanaLooschelders <dana.looschelders@uni-muenster.de>
2021-06-15 13:20:00 +0200

    initial commit

---
+++ copernicus.r
@@ -0,0 +1,14 @@
+#Copernicus data
+#load librarys
+library(sp)
+library(raster)
+library(MODIS)
+library(MODISTools)
+library(rgdal)
+library(gdalUtils)
+library(sf)
+library(mapview)
+#set directory
+setwd("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/FE_Urban_Atlas_Tree_Cover (Copernicus)")
+#read in files
+files=list.files(pattern = )
