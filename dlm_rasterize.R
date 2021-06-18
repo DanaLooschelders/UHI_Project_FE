c27c240a07087865adc681b5675551fb0d430188
ameliewe <82799931+ameliewe@users.noreply.github.com>
2021-06-18 10:54:20 +0200

    changed resolution

--- dlm_rasterize.R
+++ dlm_rasterize.R
@@ -14,16 +14,17 @@ unique(dlm_ms_all$OBJART)
 unique(dlm_ms_all$Art)
 dlm_ms_all$PolyID <- 1:nrow(dlm_ms_all)
 
+modis <- raster("/Users/amelie/Desktop/LOEK/MSc/M8/Projekt/Sciebo/Daten_roh/FE_LST/MODIS/PROCESSED/LST_Germany/MOD11A1.A2020183.LST_Day_1km.tif")
+res(modis)
+
 e <- extent(395103.5,415705.1,5744177, 5768658)
 projection <- crs( "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
 r <- raster(e,
             crs = projection)
-res(r) <- 100
+res(r) <- res(modis)
 
 #mit der Spalte OBJARt als integer (die Objekte kann man später den Zahlen zuordnen)
 # und fun=mode 
-dlm_raster<-rasterize(dlm_ms_all, r, field= as.integer(dlm_ms_all$OBJART),
-                       getCover=F, fun=Mode)
 Mode <- function(x, na.rm = FALSE) {
   if(na.rm){
     x = subset(x, !is.na(x))
@@ -32,6 +33,13 @@ Mode <- function(x, na.rm = FALSE) {
   return(ux[which.max(tabulate(match(x, ux)))])
 }    
 
+dlm_raster<-rasterize(dlm_ms_all, r, field= as.integer(dlm_ms_all$OBJART),
+                       getCover=F, fun=Mode)
 
 mapview(dlm_raster)
+
+names <- unique(dlm_ms_all$OBJART_TXT)
+nutzung <- as.data.frame(names)
+nutzung$nummer <- unique(dlm_ms_all$OBJART)
+
 writeRaster(dlm_raster,"dlm_raster", overwrite = T)
