679689332e727d31eb64e266b46aa9527da42c2a
DanaLooschelders <dana.looschelders@uni-muenster.de>
2021-06-25 12:57:18 +0200

    rename files and change indices

--- modis_pia_20200625.R
+++ modis_pia_20200625.R
@@ -3,7 +3,7 @@
 #synchronize with local hard disc
 #the sciebo symbol should appear in your file structure
 #set the working directory to the sciebo file
-setwd("C:/Users/Ready2Go/sciebo/UHI_Projekt_Fernerkundung/Orga/")
+setwd("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Orga/")
 #test to write into an dread from the file
 #whoooooo :D 
 rm(list=ls())
@@ -21,7 +21,7 @@ library(plyr)
 #### Download files ####
 #EarthdataLogin() # urs.earthdata.nasa.gov download MODIS data from LP DAAC
 
-lap = "C:/Users/Ready2Go/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/FE_LST/MODIS/"
+lap = "C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/FE_LST/MODIS/"
 MODISoptions(lap, outDirPath = file.path(lap, "PROCESSED")
              , MODISserverOrder = c("LPDAAC", "LAADS"), quiet = TRUE)
 
@@ -41,17 +41,23 @@ runGdal(job="LST_Germany","MYD11A1",begin = "2020.06.05", end = "2020.06.19",
 #    #    #    #    #    #    #    #    #    #    #    #    #    #    #    #
 ###########################################################################
 
-mainpath <- "C:/Users/Ready2Go/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/FE_LST"
+mainpath <- "C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/FE_LST"
 #datapath <- paste0(mainpath,"/data/")
 #datapath <- paste0(datapath,"/RData/")
 #Shppath <- paste0(datapath,"/ShapeLayers/")
-MODISpath <- "C:/Users/Ready2Go/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/FE_LST/MODIS/"
+MODISpath <- "C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/FE_LST/Modis/"
 #StationDat <- readOGR(paste0(Shppath,"ClimateStations.shp"))
 
 aquapath <- paste0(MODISpath,"/aqua/")
 terrapath <- paste0(MODISpath,"/terra/")
 dataTable <- list()
 timeTable <- list()
+
+#test: try only for terra
+sensor <- 2
+sensorName <- "Terra"
+path=terrapath
+
 
   for (path in c(aquapath,terrapath)){
     if(path==aquapath){
@@ -65,24 +71,34 @@ timeTable <- list()
     filelist <- list.files(path,full.names = TRUE,pattern=".tif$")
     filelist_time <- filelist[grep("view_time",filelist)]
     filelist_data <- filelist[-grep("view_time",filelist)]
-
-    ###check übereinstimmung
+    #rename files with underscore
+    #library(stringr)
+    #define new names --> ONLY EXECUTE ONCE!!!!!!!
+    #new_names=sapply(X=filelist_time, FUN=function(x) str_replace_all(string=substr(x,1,nchar(x)-4),
+                         #pattern="[.]",replacement="_"))
+    #new_names=paste(new_names, substr(filelist_time,start=nchar(filelist_time)-3, 
+                                     # stop=nchar(filelist_time))
+                    #,sep = "")
+      #rename files
+    #file.rename(from = filelist_time, to=new_names)
+    ###check ?bereinstimmung
     if (any(substr(filelist_data,nchar(filelist_data)-10,nchar(filelist_data))!=
             substr(filelist_time,nchar(filelist_time)-10,nchar(filelist_time)))){
+ 
       # stop("filelist data does not match filelist time")
-      filelist_data_night <- filelist_data[lapply(strsplit(filelist_data,"_"),function(x){x[[9]]})=="Night"]
-      filelist_data_day <- filelist_data[lapply(strsplit(filelist_data,"_"),function(x){x[[9]]})=="Day"]
-      filelist_time_night <- filelist_time[lapply(strsplit(filelist_time,"_"),function(x){x[[8]]})=="Night"]
-      filelist_time_day <- filelist_time[lapply(strsplit(filelist_time,"_"),function(x){x[[8]]})=="Day"]
+      filelist_data_night <- filelist_data[lapply(strsplit(filelist_data,"_"),function(x){x[[6]]})=="Night"]
+      filelist_data_day <- filelist_data[lapply(strsplit(filelist_data,"_"),function(x){x[[6]]})=="Day"]
+      filelist_time_night <- filelist_time[lapply(strsplit(filelist_time,"_"),function(x){x[[7]]})=="Night"]
+      filelist_time_day <- filelist_time[lapply(strsplit(filelist_time,"_"),function(x){x[[7]]})=="Day"]
       
-      filelist_data_night <- filelist_data_night[lapply(strsplit(filelist_data_night,"_"),function(x){x[[11]]})%in%
-                                                   lapply(strsplit(filelist_time_night,"_"),function(x){x[[11]]})]
-      filelist_time_night <- filelist_time_night[lapply(strsplit(filelist_time_night,"_"),function(x){x[[11]]})%in%
-                                                   lapply(strsplit(filelist_data_night,"_"),function(x){x[[11]]})]
-      filelist_data_day <- filelist_data_day[lapply(strsplit(filelist_data_day,"_"),function(x){x[[11]]})%in%
-                                               lapply(strsplit(filelist_time_day,"_"),function(x){x[[11]]})]
-      filelist_time_day <- filelist_time_day[lapply(strsplit(filelist_time_day,"_"),function(x){x[[11]]})%in%
-                                               lapply(strsplit(filelist_data_day,"_"),function(x){x[[11]]})]
+      filelist_data_night <- filelist_data_night[lapply(strsplit(filelist_data_night,"_"),function(x){x[[6]]})%in%
+                                                   lapply(strsplit(filelist_time_night,"_"),function(x){x[[7]]})]
+      filelist_time_night <- filelist_time_night[lapply(strsplit(filelist_time_night,"_"),function(x){x[[7]]})%in%
+                                                   lapply(strsplit(filelist_data_night,"_"),function(x){x[[6]]})]
+      filelist_data_day <- filelist_data_day[lapply(strsplit(filelist_data_day,"_"),function(x){x[[6]]})%in%
+                                               lapply(strsplit(filelist_time_day,"_"),function(x){x[[7]]})]
+      filelist_time_day <- filelist_time_day[lapply(strsplit(filelist_time_day,"_"),function(x){x[[7]]})%in%
+                                               lapply(strsplit(filelist_data_day,"_"),function(x){x[[6]]})]
       
       filelist_time<-c(filelist_time_day,filelist_time_night)
       filelist_data<-c(filelist_data_day,filelist_data_night)
@@ -93,5 +109,8 @@ timeTable <- list()
   }
 
 
+#save rData for aqua
+save.image(file="aqua_lists.rData")
 
-
+#save rData for terra
+save.image(file="terra_lists.rData")
