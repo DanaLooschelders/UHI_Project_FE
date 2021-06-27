d4620eb77656395494685b159c9a825183320fc7
DanaLooschelders <dana.looschelders@uni-muenster.de>
2021-06-27 11:58:50 +0200

    convert LST to MESZ

--- modis_dana_20210626.r
+++ modis_dana_20210626.r
@@ -15,24 +15,32 @@ load("C:/00_Dana/Uni/2. Mastersemester/Fernerkungsprojekt/UHI_Project_FE/aqua_li
 #terra
 load("C:/00_Dana/Uni/2. Mastersemester/Fernerkungsprojekt/UHI_Project_FE/terra_lists.rData")
 
-#load LST data
+#load LST data for day
 data_day <- lapply(filelist_data_day, raster)
+#set names
+names(data_day)<-substr(filelist_data_day,78,93)
+#load LST data for night
 data_night <- lapply(filelist_data_night, raster)
+#set names
+names(data_night)<-substr(filelist_data_night,78,93)
 #convert values to celsius
 data_day_celsius <- lapply(data_day, function(x) x-273.15)
 data_night_celsius <- lapply(data_night, function(x) x-273.15)
 
-#load time data
+#load time data for day
 time_day <- lapply(filelist_time_day, raster)
+#set names
+names(time_day)<-substr(filelist_time_day,78,93)
+#load time data for night
 time_night <- lapply(filelist_time_night, raster)
-#convert values from local solar time to UTC
-#longitude of Muenster is 7.6261
+#set names
+names(time_night)<-substr(filelist_time_night,78,93)
 
 #load MS shape
 gadm <- getData('GADM', country='DEU', level=2)
 gadm <- gadm[gadm$NAME_2=="Münster",]
 gadm_sf <- as(gadm,"sf")
-mapview(gadm_sf)
+#mapview(gadm_sf)
 
 #coordinate transformation
 data_day_celsius=lapply(data_day_celsius,  function(x) projectRaster(from=x, crs=crs(gadm)))
@@ -49,6 +57,45 @@ time_night_crop=lapply(time_night,  function(x) crop(x=x, y=gadm_sf))
 #check 
 #mapview(data_day_celsius_crop[[5]])
 
+
+####Info for solar time
+#Note that the Day_view_time and Night_view_time are in local solar time, 
+#which is the UTC time plus grid’s longitude  in  degrees  /  15  degrees  
+#(in hours,  +24  if  local  solar  time<  0  or -24  
+#if local  solar  time  >=  24). The  data  day  in  the  name  of  all  
+#the  daily  MOD11A1  files  is  in UTC so the data day in local solar
+#time at each grid may be different from the data day in UTC by one day
+
+#convert values from local solar time to UTC
+#longitude of Muenster is 7.6261
+range(values(time_day_crop[[5]]), na.rm=T)
+range(values(time_day_crop[[7]]), na.rm=T)
+#as the range in values is smaller than the temporal resolution of the training data
+#a single value is assigned to every dataset
+#LST = UTC + longitude/15
+#UTC = LST - longitude/15
+#use min value (beginn of swath) 
+UTC_time_day=rep(NA, length(time_day_crop))
+UTC_time_night=rep(NA, length(time_night_crop))
+
+for (i in 1:length(time_day_crop)){
+  #check if time values are available
+  if(sum(!is.na(values(time_day_crop[[i]])))){
+  #get minimum local solar time value
+  LST=min(values(time_day_crop[[i]]), na.rm=T)
+  #calculate UTC 
+  UTC=LST-7.6261/15
+  #convert to UTC+2 (European summer time)
+  MESZ=UTC+2
+  #convert from decimal format into POSixct
+  MESZ_format <- paste(floor(MESZ), 
+                       round((MESZ-floor(MESZ))*60), sep=":")
+  #write into results data frame
+  UTC_time_day[i]<-MESZ_format
+  }else{}
+}
+
+UTC_time_day
 #stack time and data for day
 day_stack<-data_day_celsius_crop
 for(i in 1:length(data_day_celsius_crop)){
