654c6c92e6950157d8c03f8fe062aec916e3a4e9
DanaLooschelders <dana.looschelders@uni-muenster.de>
2021-07-02 21:28:01 +0200

    plot aoa

--- model_visualization_validation.r
+++ model_visualization_validation.r
@@ -19,10 +19,13 @@ plot_ffs(model_ffs, type="selected")
 #calculate AOA
 test_aoa<-aoa(pred_stack, model_ffs)
 test_aoa$AOA[test_aoa$AOA==1]
-spplot(test_aoa$AOA)
+spplot(test_aoa, zcol="AOA")
 
-spplot(test_aoa$AOA,col.regions=c("black","white"))
+spplot(ratify(test_aoa$AOA), col.regions=c("black","white"))
 
+ncell(test_aoa$AOA[test_aoa$AOA==1])/ncell(test_aoa$AOA)*100
+
+spplot(test_aoa_factor$AOA)
 confusionMatrix(factor(model_ffs$pred),factor(model_ffs$pred))
 
 model_ffs$modelType
@@ -32,3 +35,20 @@ model_ffs$selectedvars
 model_ffs$selectedvars_perf_SE
 model_ffs$metric
 model_ffs$selectedvars_perf
+
+#plot areas inside AOA
+test_predict[test_aoa$AOA == 0] <- NA
+
+map <- tm_shape(test_predict,
+                raster.downsample = FALSE) +
+  tm_raster(title = "LUC")+
+  tm_scale_bar(bg.color="white")+
+  tm_grid(n.x=4,n.y=4,projection="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")+
+  tm_layout(legend.position = c("left","bottom"),
+            legend.bg.color = "white",
+            bg.color="black",
+            legend.bg.alpha = 0.8)+
+  tm_add_legend(type = "fill",
+                col="black",
+                labels = "Outside AOA")
+map
