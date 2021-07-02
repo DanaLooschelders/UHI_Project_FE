e1cfdbc58dd92e301a6d9957c66886acc9ab9419
ameliewe <82799931+ameliewe@users.noreply.github.com>
2021-07-02 14:15:19 +0200

    projection

--- dlm_rasterize.R
+++ dlm_rasterize.R
@@ -17,9 +17,7 @@ gadm_sf <- as(gadm,"sf")
 crs(dlm_ms_all) 
 
 unique(dlm_ms_all$OBJART)
-unique(dlm_ms_all$Art)
 dlm_ms_all$PolyID <- 1:nrow(dlm_ms_all)
-
 
 e <- extent(395103.5,415705.1,5744177, 5768658)
 projection <- crs(gadm)
@@ -37,16 +35,27 @@ Mode <- function(x, na.rm = FALSE) {
   return(ux[which.max(tabulate(match(x, ux)))])
 }    
 
-dlm_raster <- rasterize(dlm_ms_all, r, field= as.integer(dlm_ms_all$OBJART),
-                       getCover=F, fun=Mode)
+getmode <- function(v,na.rm) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}   
+
+
+dlm_raster <- rasterize(dlm_ms_all, r, field= as.numeric(dlm_ms_all$OBJART),
+                        getCover=F, fun=Mode)
+#dlm_raster <- fasterize(dlm_ms_all, r, 
+ #                       by = as.integer("dlm_ms_all$OBJART"),background=0)
+
+setwd("/Users/amelie/Desktop/LOEK/MSc/M8/Projekt/Sciebo/Daten_bearbeitet/dlm")
+writeRaster(dlm_raster,"dlm_raster.tif", overwrite = T)
 
 mapview(dlm_raster)
-
-dlm_raster <- projectRaster(from=dlm_raster, crs=crs(gadm))
+unique(dlm_raster)
+dlm_raster_proj <- projectRaster(from=dlm_raster, crs ="+proj=longlat +datum=WGS84 +no_defs "  )
 
 names <- unique(dlm_ms_all$OBJART_TXT)
 nutzung <- as.data.frame(names)
 nutzung$nummer <- unique(dlm_ms_all$OBJART)
 
 setwd("/Users/amelie/Desktop/LOEK/MSc/M8/Projekt/Sciebo/Daten_bearbeitet/dlm")
-writeRaster(dlm_raster,"dlm_raster_100m.tif", overwrite = T)
+writeRaster(dlm_raster_proj,"dlm_raster_100m.tif", overwrite = T)
