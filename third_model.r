544af17b14a807dfe17573f5598713a8887f1d13
DanaLooschelders <dana.looschelders@uni-muenster.de>
2021-08-01 08:40:33 +0200

    prep 3 model

--- third_model.r
+++ third_model.r
@@ -5,4 +5,7 @@
       #Steinf: Temp, rH, wind, stability (calculated from temp)
   #LCZ (disaggregate to 10m)
   #Tree cover (10m)
-
+setwd("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_bearbeitet/Copernicus")
+#read in 10m Copernicus data
+raster("copernicus_tree_cover_crop_MS_10m.tif")
+#prep training data: logger and netatmo (30min res)
