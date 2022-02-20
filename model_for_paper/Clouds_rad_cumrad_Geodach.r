e46baf80a0693df4808b35cc42d479e9f679f140
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-02-20 08:11:35 +0100

    remove 2019 timespan

--- model_for_paper/Clouds_rad_cumrad_Geodach.r
+++ model_for_paper/Clouds_rad_cumrad_Geodach.r
@@ -4,18 +4,18 @@ library(lubridate)
 library(dplyr)
 library(readxl)
 #read in 2019 data
-geo_2019<-read.table("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/Meteo_Daten/Meteo_GeoDach/GeoDach_2019.csv",
-                     sep=",", dec=".", skip=2000, header=F, nrow=20000,fill = T, na.strings = c("-"),
-                     col.names = read.table("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/Meteo_Daten/Meteo_GeoDach/GeoDach_2019.csv",
-                                            sep=",", dec=".", skip=0, header=F, nrow=1))
+#geo_2019<-read.table("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/Meteo_Daten/Meteo_GeoDach/GeoDach_2019.csv",
+#                     sep=",", dec=".", skip=2000, header=F, nrow=20000,fill = T, na.strings = c("-"),
+#                     col.names = read.table("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/Meteo_Daten/Meteo_GeoDach/GeoDach_2019.csv",
+#                                            sep=",", dec=".", skip=0, header=F, nrow=1))
 #subset to parameters needed
-geo_2019<-geo_2019[,c(1,16,23)]
+#geo_2019<-geo_2019[,c(1,16,23)]
 
 #change colnames
-colnames(geo_2019)[1] <- "date"
+#colnames(geo_2019)[1] <- "date"
 #format timestamp 1: 2019-08-14 19:10:00
-geo_2019$date<-strptime(geo_2019$date, format="%Y-%m-%d %H:%M:%S", tz="Europe/London") #tz is russiun solution =eternal wintertime
-geo_2019$date<-as.POSIXct(geo_2019$date, tz="Europe/London")
+#geo_2019$date<-strptime(geo_2019$date, format="%Y-%m-%d %H:%M:%S", tz="Europe/London") #tz is russiun solution =eternal wintertime
+#geo_2019$date<-as.POSIXct(geo_2019$date, tz="Europe/London")
 
 #read in 2020 data
 geo_2020<-read.table("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Daten_roh/Meteo_Daten/Meteo_GeoDach/GeoDach2020_withcloudcover.csv",
@@ -53,31 +53,32 @@ meteo_geo <- with_tz(time = meteo_geo, tzone = "CET")
 #07/03 - 07/31          2020-07-03 00:00:00 bis 2020-07-31 00:00:00
 
 #choose timespans seperately
-Summer2019<-meteo_geo[meteo_geo$date>="2019-08-20 00:00:00"&
-                        meteo_geo$date<="2019-09-30 00:00:00",]
-Autumn2019<-meteo_geo[meteo_geo$date>="2019-09-24 00:00:00"&
-                        meteo_geo$date<="2019-11-09 00:00:00",]
+#Summer2019<-meteo_geo[meteo_geo$date>="2019-08-20 00:00:00"&
+#                        meteo_geo$date<="2019-09-30 00:00:00",]
+#Autumn2019<-meteo_geo[meteo_geo$date>="2019-09-24 00:00:00"&
+#                        meteo_geo$date<="2019-11-09 00:00:00",]
 
 Spring2020<-meteo_geo[meteo_geo$date>="2020-06-05 00:00:00"&
                         meteo_geo$date<="2020-06-19 00:00:00",]
 Summer2020<-meteo_geo[meteo_geo$date>="2020-07-03 00:00:00"&
                         meteo_geo$date<="2020-07-31 00:00:00",]
 #rbind together
-meteo_geo<-rbind(Summer2019,Autumn2019,Spring2020,Summer2020)
+meteo_geo<-rbind(Spring2020,Summer2020) #exclude Summer2019,Autumn2019,
 #remove temporaray dat
-rm(Summer2019,Autumn2019,Spring2020,Summer2020)
+rm(Spring2020,Summer2020) #exclude Summer2019,Autumn2019,
 ####calculate cumulative radiation per day####
 #read in sunrise/sunset data
 setwd("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Paper/Prädiktoren/Time of day")
 sunrise<-read.table(file = "Sunrise_dates.csv", sep=";", dec=".", header=T)      
 sunrise$day<-seq.Date(from=as.Date("2020-01-01"), to=as.Date("2020-12-31"), by="day")
 sunrise<-sunrise[,c(3,7)]
+sunrise$Sunrise<-as.factor(sunrise$Sunrise)
 #save with proper date
 write.csv(sunrise, "sunrise_with_date.csv", row.names = F)
 #prepare meteo geo
 meteo_geo$day<-lubridate::date(meteo_geo$date) #create column with day
 #join with meteo_geo
-meteo_geo<-left_join(meteo_geo, sunrise)
+meteo_geo<-left_join(meteo_geo, sunrise, by="day")
 meteo_geo<-meteo_geo %>% 
   group_by(Sunrise) %>% #group by sunrise --> spans from one sunrise to next
   mutate(cum_radiation = cumsum(Shortwave.Radiation)) #calculate cumsum per day
