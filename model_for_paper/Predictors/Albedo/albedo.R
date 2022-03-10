1324150eb5086072909da75d364afeac6a37a603
ameliewe <a.wendiggesen@gmail.com>
2022-03-10 12:54:18 +0100

    albedo calculations

--- model_for_paper/Predictors/Albedo/albedo.R
+++ model_for_paper/Predictors/Albedo/albedo.R
@@ -7,8 +7,10 @@ library(RStoolbox)
 
 setwd("/Users/ameliewendiggensen/sciebo/UHI_Projekt_Fernerkundung/Paper/Prädiktoren/")
 
-sen2020_06_23 <- stack("NDVI/2020_1/out/TOA/S2A1C_20200623_108_ms_TOA_10.tif")
-sen2020_06_01 <- stack("NDVI/2020_1/out/TOA/S2B1C_20200601_008_ms_TOA_10.tif")
+sen2020_06_23 <- stack("/Volumes/work/NDVI/2020_1/out/TOA/S2A1C_20200623_108_ms_TOA_10.tif")
+sen2020_06_01 <- stack("/Volumes/work/NDVI/2020_1/out/TOA/S2B1C_20200601_008_ms_TOA_10.tif")
+
+sen2020_06_01$S2B1C_20200601_008_ms_TOA_10.1
 
 sen2020_07 <- stack("NDVI/2020_2/out/TOA/S2B1C_20200731_008_ms_TOA_10.tif")
 
@@ -19,7 +21,38 @@ sen_10m_2020_07 <- stack(sen2020_07$S2B1C_20200731_008_ms_TOA_10.2,
 
 mapview(sen_10m_2020_07)
 
-#######  
+uberlay <- function(..., fun) {
+  fun <- match.fun(fun)
+  L <- lapply(list(...), unstack)
+  stack(do.call(mapply, c(FUN=function(...) calc(stack(...), fun), L)))
+}
+
+sen2020_06_mean <- uberlay(sen2020_06_01, sen2020_06_23, fun='mean')
+
+scaled_sen2020_06 <- rescaleImage(sen2020_06_mean, ymin = 0, ymax= 255)
+#oder 
+#sen2020_06_8b <- calc(sen2020_06_mean, fun=function(x){((x - min(x)) * 255)/(max(x)- min(x)) + 0})
+
+
+scaled_sen_10m_2020_06 <- stack(scaled_sen2020_06$layer.2,
+                                scaled_sen2020_06$layer.3,
+                                scaled_sen2020_06$layer.4,
+                                scaled_sen2020_06$layer.8)
+set.seed(23)
+pca_2020_06 <- rasterPCA(scaled_sen_10m_2020_06, nComp = nlayers(scaled_sen_10m_2020_06))
+pca_2020_06$map
+
+summary(pca_2020_06$model)
+loadings(pca_2020_06$model)
+
+mapview(pca_2020_06$map$PC1)
+pca_2020_06$map$PC1
+
+pca_06 <- rescaleImage(pca_2020_06$map$PC1, ymin = 0, ymax= 1)
+
+writeRaster(pca_06, "Albedo/albedo_ms_2020_06.tif")
+
+####### 8b raster ####
 #convert to 0-255 using the calc. function and basic raster algebra
 ras8b <- calc(sen2020_07, fun=function(x){((x - min(x)) * 255)/(max(x)- min(x)) + 0})
 
@@ -27,25 +60,7 @@ ras8b <- calc(sen2020_07, fun=function(x){((x - min(x)) * 255)/(max(x)- min(x)) 
 writeRaster(ras8b, 'NDVI/2020_2/out/TOA/ras8b.tif', datatype='INT1U')
 sen_8b_2020_07 <- stack("NDVI/2020_2/out/TOA/ras8b.tif")
 
-sen_8b_10m_2020_07 <- stack(sen_8b_2020_07$ras8b.2,
-                            sen_8b_2020_07$ras8b.3,
-                            sen_8b_2020_07$ras8b.4,
-                            sen_8b_2020_07$ras8b.8)
-
-plotRGB(sen_8b_2020_07, r =4, b =2, g = 3)
-
-set.seed(23)
-pca_2020_07 <- rasterPCA(sen_8b_10m_2020_07, nComp = nlayers(sen_8b_10m_2020_07),maskCheck=F)
-pca_2020_07$map
-
-summary(pca_2020_07$model)
-loadings(pca_2020_07$model)
-
-ggRGB(pca_2020_07$map,2,3,1, stretch="lin", q=0)
-mapview(pca_2020_07$map$PC1) 
-
 #########
-
 scaled_sen2020_07 <- rescaleImage(sen2020_07, ymin = 0, ymax= 255)
   
 scaled_sen_10m_2020_07 <- stack(scaled_sen2020_07$layer.2,
@@ -61,3 +76,9 @@ loadings(pca2_2020_07$model)
 
 ggRGB(pca2_2020_07$map,2,3,1, stretch="lin", q=0)
 mapview(pca2_2020_07$map$PC1)
+pca2_2020_07$map$PC1
+
+pca_07 <- rescaleImage(pca2_2020_07$map$PC1, ymin = 0, ymax= 1)
+
+writeRaster(pca_07, "Albedo/albedo_ms_2020_07.tif")
+
