bc923397952e8317b0abcd4d35a3e2d93c4f06b1
ameliewe <a.wendiggesen@gmail.com>
2022-04-19 14:21:40 +0200

    tidy up

--- model_for_paper/Predictors/time_of_day.R
+++ model_for_paper/Predictors/time_of_day.R
@@ -3,9 +3,16 @@ library(readr)
 library(dplyr)
 setwd("/Users/ameliewendiggensen/sciebo/UHI_Projekt_Fernerkundung/Paper/Prädiktoren/")
 setwd("/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Prädiktoren/")
+
+#times
+#06/05 - 06/19          2020-06-05 00:00:00 bis 2020-06-19 00:00:00
+#07/03 - 07/31          2020-07-03 00:00:00 bis 2020-07-31 00:00:00 
+
+#load data 
 sunrise_dates <- read_csv2("Time_of_day/sunrise_dates.csv")
 sunrise_w_date <- read_csv("Time_of_day/sunrise_with_date.csv")
 
+#merge and organize date 
 sunrise <- merge(sunrise_dates, sunrise_w_date, by.x = "Sunrise", by.y = "Sunrise")
 sunrise <- sunrise[-c(2,3,160,161,258,259,372,373), ] #manually remove doubled dates 
 
@@ -15,6 +22,7 @@ sorted$ID <- seq.int(nrow(sorted))
 times_06 <- sorted[sorted$ID %in% c(157:171), ]
 times_07 <- sorted[sorted$ID %in% c(185:213), ]
 
+#add hourly information
 hourly <- data.frame(seq(ISOdate(2020,06,05, hour = 0),ISOdate(2020,06,19, hour = 23),by = "hour"))
 names(hourly) <- c("date")
 hourly <- hourly %>% mutate(day = substr(date,1L,10L))
@@ -24,6 +32,7 @@ hourly_times_06 <- merge(times_06, hourly, by.x = "day", by.y = "day")
 
 hourly_times_06$time <- format(as.POSIXct(hourly_times_06$date), format = "%H:%M:%S")
 
+#as posixct 
 library(hms) 
 hourly_times_06$time<-as_hms(hourly_times_06$time) 
 class(hourly_times_06$time) 
@@ -32,12 +41,16 @@ as.POSIXct(hourly_times_06$date,"%Y-%m-%d %H:%M:%S")
 as.POSIXct(hourly_times_06$Sunset,"%H:%M:%S")
 as.POSIXct(hourly_times_06$Sunrise,"%H:%M:%S")
 
-time_s_sunrise <-  ifelse(as.numeric(hourly_times_06$time) >= as.numeric(hourly_times_06$Sunrise) & (as.numeric(hourly_times_06$time) < as.numeric(hourly_times_06$Sunset)),
+#time since sun rise 
+hours_ssr <-  ifelse(as.numeric(hourly_times_06$time) >= as.numeric(hourly_times_06$Sunrise) & (as.numeric(hourly_times_06$time) < as.numeric(hourly_times_06$Sunset)),
                 difftime(hourly_times_06$time,hourly_times_06$Sunrise,units="hours"),
                 NA) 
-#Danas Version
-hourly_times_06$hours_sss<-NA #sss = since sun set
+
+hourly_times_06$hours_ssr <- hours_ssr
+
 #time since sunset
+hourly_times_06$hours_sss<-NA 
+
 for(i in 24:nrow(hourly_times_06)){ #start after first day
   if(hourly_times_06$time[i]>hourly_times_06$Sunset[i]){ #if it is after sunset the same day
     hourly_times_06$hours_sss[i]<-difftime(hourly_times_06$time[i],hourly_times_06$Sunset[i],units="hours")
@@ -46,13 +59,3 @@ for(i in 24:nrow(hourly_times_06)){ #start after first day
     hourly_times_06$hours_sss[i]<-difftime(hourly_times_06$time[i],sunsettime,units="hours")+24#calculate hours
   }
 } 
- 
-time_s_sunset <-  ifelse(as.numeric(hourly_times_06$time) <= as.numeric(hourly_times_06$Sunrise) | (as.numeric(hourly_times_06$time) > as.numeric(hourly_times_06$Sunset)),
-                         difftime(hourly_times_06$time,hourly_times_06$Sunset,units="hours"),
-                         NA)
-
-hourly_times_06$time_s_sunrise <- time_s_sunrise
-hourly_times_06$time_s_sunset <- time_s_sunset
-
-#06/05 - 06/19          2020-06-05 00:00:00 bis 2020-06-19 00:00:00
-#07/03 - 07/31          2020-07-03 00:00:00 bis 2020-07-31 00:00:00
