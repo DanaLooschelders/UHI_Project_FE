ce9a13630576ccd0cb7f2f05ff7933c07d33d5f4
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-05-06 21:58:19 +0200

    working for-loop

---
+++ model_for_paper/Predictors/for_loop_for_lidar.r
@@ -0,0 +1,14 @@
+#as for loop
+setwd("C:/00_Dana/Uni/2. Mastersemester/Fernerkungsprojekt/Paper/gml/gml")
+#list files
+files<-list.files(pattern=".gml")
+#create empty files list
+files_list<-vector(mode='list', length=length(files))
+#set names of list to match files
+names(files_list)<-files
+#loop through all the files
+for(i in files){
+  tryCatch({
+    files_list[[i]]<-read_sf(i, layer="Building")
+  })
+}
