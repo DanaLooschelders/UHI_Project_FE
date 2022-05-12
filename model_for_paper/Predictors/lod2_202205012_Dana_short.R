adf9d760f882924a8b13dc63e715da455cfeba4f
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-05-12 16:34:19 +0200

    testversion

---
+++ model_for_paper/Predictors/lod2_202205012_Dana_short.R
@@ -0,0 +1,34 @@
+library(stars)
+library(sf)
+library(raster)
+library(mapview)
+
+#set working directory
+setwd("C:/00_Dana/Uni/2. Mastersemester/Fernerkungsprojekt/Paper/gml/gml")
+
+#list files
+files<-list.files(pattern=".gml")
+#create empty files list
+files_list<-vector(mode='list', length=length(files))
+#set names of list to match files
+names(files_list)<-files
+
+#loop through all the files
+for(i in files){
+  tryCatch({
+    #read in files and set crs
+    files_list[[i]]<-read_sf(i, layer="Building",
+                             crs="+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs")
+    
+  })
+}
+#raster to stars Raster with pixel size of 10x10
+raster_list<-lapply(files_list, function(x)  st_rasterize(x["measuredHeight"], dx=10, dy=10 ))
+#coerce to sp object
+sp_list<-lapply(raster_list, function(x) as(x, "Spatial"))
+#test by plotting
+plot(sp_list[[1]], axes=T)
+mapview(sp_list[[1]])
+#plot multiple
+mapview(sp_list[[9]])+mapview(sp_list[[8]])+mapview(sp_list[[7]])
+
