491c2b2c37244dd59813e8a84fa629938fae68f6
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-05-18 21:33:36 +0200

    yeah! mit den richtigen Höhen!

--- model_for_paper/Predictors/lod2_202205018_Dana_short.R
+++ model_for_paper/Predictors/lod2_202205018_Dana_short.R
@@ -43,18 +43,28 @@ length(Filter(is.null, files_list))
 
 #delete NULL files
 files_list<-files_list[vapply(files_list, Negate(is.null), NA)]
-
+#some files have a column named "measuredHeight_"
+check<-sapply(files_list, function(x) exists("measuredHeight_", x))
+names<-names(check[check=="TRUE"])
 #raster to stars Raster with pixel size of 10x10
-raster_list<-lapply(files_list, function(x)  st_rasterize(x["measuredHeight"], dx=10, dy=10 ))
+raster_list<-lapply(files_list, 
+                    function(x)  st_rasterize(x["measuredHeight"], 
+                                              dx=10, dy=10 ))
+#raster to stars raster for file with special name
+for(i in names){
+  raster_list[[i]]<-st_rasterize(files_list[[i]]["measuredHeight_"],
+                                 dx=10, dy=10)
+}
 #coerce to sp object
 sp_list<-lapply(raster_list, function(x) as(x, "Spatial"))
 #test by plotting
 plot(sp_list[[1]], axes=T)
 mapview(sp_list[[1]])
 #plot multiple
-mapview(sp_list[[9]])+mapview(sp_list[[8]])
-#coerce to raster
+mapview(sp_list[[9]])+mapview(sp_list[[10]])#coerce to raster
 raster_list <- lapply(sp_list, raster)
+#check
+mapview(raster_list[[250]])
 #check resolution
 lapply(raster_list, res)
 #mosaic together
@@ -67,8 +77,17 @@ raster_list.mosaicargs$na.rm <- TRUE #ignore NA values
 ras_mosaic <- do.call(mosaic,  raster_list.mosaicargs) #mosaic with do.call
 
 mapview(ras_mosaic) #check
+range(values(ras_mosaic), na.rm=T)
+hist(values(ras_mosaic))
 
 #calculate standard deviation of building height
 sd_3x3<-focal(ras_mosaic, w=matrix(1, 3,3), fun=sd, na.rm=T)
-
 mapview(sd_3x3)#check
+
+sd_5x5<-focal(ras_mosaic, w=matrix(1, 5,5), fun=sd, na.rm=T)
+mapview(sd_5x5)
+#write to file
+setwd("C:/Users/Dana/sciebo/UHI_Projekt_Fernerkundung/Prädiktoren/lidar")
+writeRaster(ras_mosaic, "Lidar_building_height")
+writeRaster(sd_3x3, "Lidar_building_sd_3x3")
+writeRaster(sd_5x5, "Lidar_building_sd_5x5")
