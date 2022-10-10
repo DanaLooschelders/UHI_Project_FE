a42b6178161c60f52a8352e691a771b5c461985e
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-10-10 14:37:06 +0200

    initial commit

---
+++ model_for_paper/Predictors/planetscope_NDVI.R
@@ -0,0 +1,38 @@
+#NDVI with Planetscope
+library(raster)
+library(sp)
+library(rgdal)
+library(mapview)
+setwd("D:/ms2020new_psscene_analytic_sr_udm2/files")
+
+#load file names
+files<-list.files(pattern="harmonized_clip.tif")
+files_list<-vector(mode='list', length=length(files))
+names(files_list)<-files
+
+#read files into list
+for(i in files){
+  tryCatch({
+    print(i)
+    #read in files and set crs
+    files_list[[i]]<-stack(i)
+  }, error=function(e){message("Caught an error")})
+}
+
+#plot
+spplot(files_list[[7]])
+#merge
+files_list$fun <- "mean"
+mos <- do.call(mosaic, files_list) #that does not work, I have no idea why
+#ugly, but it works
+test<-mosaic(files_list[[1]], files_list[[2]], files_list[[3]], files_list[[4]],
+            files_list[[5]], files_list[[6]], files_list[[7]],  fun="mean")
+spplot(test) #plot
+
+mapview(test)
+
+#calculate NDVI
+#Formula: (NIR – R) / (NIR + R) 
+NDVI<-(test[[4]]-test[[3]])/(test[[4]]+test[[3]])
+
+mapview(NDVI)
